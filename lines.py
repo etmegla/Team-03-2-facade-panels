94af4ab086bc42e908281bbcb45bcf8464f1a0a0
etmegla <etmaglari@gmail.com>
2026-03-05 08:08:47 -0500

    lines.py

---
+++ lines.py
@@ -0,0 +1,50 @@
+import compute_rhino3d.Util
+import compute_rhino3d.Grasshopper as gh
+import rhino3dm
+import json
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+compute_url = os.getenv("COMPUTE_URL", "https://compute8.iaac.net/").rstrip("/") + "/"
+compute_api_key = os.getenv("COMPUTE_API_KEY", "")
+
+if not compute_api_key:
+    raise RuntimeError("COMPUTE_API_KEY is missing. Set it in .env or environment variables.")
+
+compute_rhino3d.Util.url = compute_url
+compute_rhino3d.Util.apiKey = compute_api_key
+
+pt1 = rhino3dm.Point3d(0, 0, 0)
+circle = rhino3dm.Circle(pt1, 5)
+angle = 20
+
+# convert circle to curve and stringify
+curve = json.dumps(circle.ToNurbsCurve().Encode())
+
+# create list of input trees
+curve_tree = gh.DataTree("curve")
+curve_tree.Append([0], [curve])
+rotate_tree = gh.DataTree("rotate")
+rotate_tree.Append([0], [angle])
+trees = [curve_tree, rotate_tree]
+
+output = gh.EvaluateDefinition('twisty.gh', trees)
+print(output)
+
+# decode results
+branch = output['values'][0]['InnerTree']['{0;0}']
+lines = [rhino3dm.CommonObject.Decode(json.loads(item['data'])) for item in branch]
+
+filename = 'twisty.3dm'
+
+print('Writing {} lines to {}'.format(len(lines), filename))
+
+# create a 3dm file with results
+model = rhino3dm.File3dm()
+for l in lines:
+    model.Objects.AddCurve(l) # they're actually LineCurves...
+
+model.Write(filename)
